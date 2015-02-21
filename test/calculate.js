73592c2386e54b1cd537a8c7d214995612546cb9
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-21 01:33:53 -0500

    update tests

--- test/calculate.js
+++ test/calculate.js
@@ -9,26 +9,53 @@
 
 require('should');
 var Context = require('..');
-var context = new Context();
+var app = new Context();
 
 describe('.calculate()', function() {
-  it('should calculate the context based on the provided levels.', function() {
-    context
+  beforeEach(function() {
+    app
       .setContext('a', 1)
       .setContext('b', 2)
       .setContext('c', 3)
-      .setContext('c', 4)
-      .setContext('e', 5);
 
-    context.extendContext('a', {aaa: 'aaa'});
-    context.extendContext('a', {bbb: 'bbb'});
-    context.extendContext('b', {ccc: 'cdd'});
-    context.extendContext('b', {ddd: 'ddd'});
-    context.extendContext('d', {eee: 'eee'});
-    context.extendContext('e', {fff: 'fff'});
-    console.log(context)
+    app.extendContext('a', {foo: 'aaa', one: 'two'});
+    app.extendContext('b', {foo: 'bbb', bar: 'BBB'});
+    app.extendContext('c', {foo: 'ccc', bar: 'CCC'});
+  });
 
-    var res = context.calculate(['a', 'b']);
-    console.log(res)
+  it('should calculate the context for a single level.', function() {
+    app.calculate('a').should.eql({foo: 'aaa', one: 'two'});
+  });
+
+  it('should calculate the context for an array of levels.', function() {
+    app.calculate(['a']).should.eql({foo: 'aaa', one: 'two'});
+    app.calculate(['a', 'b']).should.eql({foo: 'bbb', one: 'two', bar: 'BBB'});
+    app.calculate(['a', 'c']).should.eql({foo: 'ccc', one: 'two', bar: 'CCC'});
+    app.calculate(['a', 'b', 'c']).should.eql({foo: 'ccc', one: 'two', bar: 'CCC'});
+  });
+
+  it('should calculate the context for all levels.', function() {
+    app.calculate().should.eql({foo: 'ccc', one: 'two', bar: 'CCC'});
+  });
+
+  it('should calculate in order of the provided keys:', function() {
+    app.calculate(['a']).should.eql({foo: 'aaa', one: 'two'});
+    app.calculate(['b', 'a']).should.eql({foo: 'aaa', one: 'two', bar: 'BBB'});
+    app.calculate(['c', 'a']).should.eql({foo: 'aaa', one: 'two', bar: 'CCC'});
+    app.calculate(['c', 'b']).should.eql({foo: 'bbb', bar: 'BBB'});
+  });
+
+  it('should use a custom sorting function.', function() {
+    app.calculate(function(a, b) {
+      return app.lvl[a] - app.lvl[a];
+    }).should.eql({foo: 'ccc', one: 'two', bar: 'CCC'});
+
+    app.calculate(['a', 'b'], function(a, b) {
+      return app.lvl[a] - app.lvl[a];
+    }).should.eql({foo: 'bbb', one: 'two', bar: 'BBB'});
+
+    app.calculate(['a', 'b'], function(a, b) {
+      return app.lvl[b] - app.lvl[a];
+    }).should.eql({foo: 'aaa', one: 'two', bar: 'BBB'});
   });
 });
